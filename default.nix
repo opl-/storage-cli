ff65fc07a6eabd508871d0bbc09192612e4fe07c
opl <4833621+opl@users.noreply.github.com>
2025-06-16 14:35:17 +0200

    Update package-lock.json

--- default.nix
+++ default.nix
@@ -6,5 +6,5 @@ pkgs.buildNpmPackage {
   src = ./.;
 
   # Obtained with `prefetch-npm-deps package-lock.json`
-  npmDepsHash = "sha256-mSIPtmyL46rYQoc50jArNbYp43EZAzfJVkeHQgD+hNc=";
+  npmDepsHash = "sha256-T6UuVlGs2DUvgKZnizcHTGYqdy16fGA/zR4cHG2UQ5Q=";
 }
